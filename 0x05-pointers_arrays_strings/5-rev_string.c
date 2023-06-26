27fbcb4f799424b1e28060860b03b8bf467225f0
AfiaGreat <ngondweni@gmail.com>
2023-06-26 16:50:31 -0700

    pushing header #5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string
+ * @s: Pointer to a string
+ */
+void rev_string(char *s)
+{
+	int length = 0;
+	int i;
+	char temp;
+
+	/* Calculate the length of the string */
+	while (s[length] != '\0')
+		length++;
+
+	/* Swap characters from the beginning and end of the string */
+	for (i = 0; i < length / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[length - 1 - i];
+		s[length - 1 - i] = temp;
+	}
+}
+
