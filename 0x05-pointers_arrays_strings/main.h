2eb7725d8b8f261c88798f0e6121f7ba3f26c059
AfiaGreat <ngondweni@gmail.com>
2023-06-26 17:32:02 -0700

    pushing header #9

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 void puts_half(char *str);
+void print_array(int *a, int n);
 
 #endif /* MAIN_H */
