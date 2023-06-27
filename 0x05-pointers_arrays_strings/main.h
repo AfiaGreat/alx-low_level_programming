b5c62c8d17ac5ff8e1fb3ef471f0c5b110e91462
AfiaGreat <ngondweni@gmail.com>
2023-06-26 17:39:10 -0700

    advanced questions

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char *_strcpy(char *dest, char *src);
-void puts_half(char *str);
+int _putchar(char c);
+void _puts(char *str);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
 void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
 #endif /* MAIN_H */
+
